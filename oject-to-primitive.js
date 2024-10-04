83c7162f23ee32b65d45565a67c456e11dd490d0
petia duskin <petr.dyuskin@mail.ru>
2024-10-04 19:56:44 +0500

    add main methods for casting object to primitives

--- oject-to-primitive.js
+++ oject-to-primitive.js
@@ -15,3 +15,24 @@ obj[Symbol.toPrimitive] = function (hint) {
 console.log(obj)
 console.log(+obj) // 203201
 console.log(String(obj)) // Name
+
+
+// Если нету Symbol.toPrimitive, то js пытается найти методы toString и valueOf
+let obj2 = {
+    name: "Name",
+    age: 25
+}
+
+console.log(String(obj2)) // [object Object]
+console.log(obj2.valueOf()) // { name: 'Name', age: 25 }
+
+obj2.toString = function () {
+    return this.name;
+}
+
+obj2.valueOf = function () {
+    return this.age;
+}
+
+console.log(String(obj2)) // Name
+console.log(Number(obj2)) // 25
